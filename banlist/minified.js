e56ef6b8ac6b67f1e32ef107e1caa2c797ad0114
Gera Janos <gerajanos@gmail.com>
2011-11-07 23:38:40 +0100

    remove test username from minified banlist script

--- banlist/minified.js
+++ banlist/minified.js
@@ -8,7 +8,7 @@
 // akiket ki akarsz tiltani a témából. Szóközt ne használj!
 // Példa: var bl_users = 'név1,név2,név3';
 
-var bl_users = 'Ability';
+var bl_users = '';
 
 // EZT A RÉSZT NE MÓDOSÍTSD !!!
 $(document).ready(function(){bl_users=bl_users.split(',');$('.topichead').each(function(){var a=$(this).find('a[href*="forumuserinfo.php"] img').length?$(this).find('a[href*="forumuserinfo.php"] img').attr('title'):$(this).find('a[href*="forumuserinfo.php"]').html();a=a.replace(" - VIP","");if(bl_users.indexOf(a)!=-1){$(this).parents('center:first').hide()}})});
