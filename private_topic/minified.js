ad3c20493f616e4240cea122252c6ba0a28f85b7
Gera Janos <gerajanos@gmail.com>
2011-11-08 00:18:09 +0100

    private_topic code added

---
+++ private_topic/minified.js
@@ -0,0 +1,15 @@
+// SG.hu topic private_topic v1.0 20111107
+// Készítette: dzsani
+// Inspiráció: cSuwwi - Drom - http://drom.hu/
+
+
+// Felhasználók fehérlistája
+// Vesszõvel elválasztva sorold fel azokat a felhasználóneveket
+// akiknek engedélyezed a topikba lépést. Szóközt ne használj!
+// Példa: var pt_users = 'név1,név2,név3';
+
+var pt_users = '';
+
+
+// EZT A RÉSZT NE MÓDOSÍTSD !!!
+$(document).ready(function(){var a=$('.std1:eq(1)').html().replace("Bejelentkezve: ","");if(pt_users.indexOf(a)==-1){alert('Ez egy privát téma, nincs jogosultságod az olvasásához!');history.go(-1)}});
---
+++ private_topic/source.js
@@ -0,0 +1,25 @@
+// SG.hu topic private_topic v1.0 20111107
+// Készítette: dzsani
+// Inspiráció: cSuwwi - Drom - http://drom.hu/
+
+
+// Felhasználók fehérlistája
+// Vesszõvel elválasztva sorold fel azokat a felhasználóneveket
+// akiknek engedélyezed a topikba lépést. Szóközt ne használj!
+// Példa: var pt_users = 'név1,név2,név3';
+
+var pt_users = '';
+
+
+// EZT A RÉSZT NE MÓDOSÍTSD !!!
+$(document).ready(function() {
+	
+	// Get nickname
+	var nick = $('.std1:eq(1)').html().replace("Bejelentkezve: ","");
+	
+	// Check user
+	if(pt_users.indexOf(nick) == -1) {
+		alert('Ez egy privát téma, nincs jogosultságod az olvasásához!');
+		history.go(-1);
+	}
+});
