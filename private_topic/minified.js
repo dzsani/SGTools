0e51dfe852dc634530a02266433077f0a6fd5530
Gera Janos <gerajanos@gmail.com>
2011-11-08 14:08:11 +0100

    added blacklist to private_topic

--- private_topic/minified.js
+++ private_topic/minified.js
@@ -1,7 +1,15 @@
-// SG.hu topic private_topic v1.0 20111107
+// SG.hu private_topic v1.0 20111107
 // Készítette: dzsani
 // Inspiráció: cSuwwi - Drom - http://drom.hu/
 
+// Mûködési mód
+// 0: fehérlista, 1: feketelista
+// Ha nullán hagyod, akkor azok írhatnak a topikba,
+// akiket felsorolsz a lentebbi névlistába.
+// Ha egyre állítod, akkor mindenki írhat, kivéve
+// a lentebb felsorolt felhasználók.
+
+var pt_mode = 1;
 
 // Felhasználók fehérlistája
 // Vesszõvel elválasztva sorold fel azokat a felhasználóneveket
@@ -12,4 +20,4 @@ var pt_users = '';
 
 
 // EZT A RÉSZT NE MÓDOSÍTSD !!!
-$(document).ready(function(){pt_users=pt_users.split(',');var a=$('.std1:eq(1)').html().replace("Bejelentkezve: ","");if(pt_users.indexOf(a)==-1){alert('Ez egy privát téma, nincs jogosultságod az olvasásához!');history.go(-1)}});
+$(document).ready(function(){pt_users=pt_users.split(',');var a=$('.std1:eq(1)').html().replace("Bejelentkezve: ","");if(pt_mode==0){if(pt_users.indexOf(a)==-1){pt_redirectBack()}}else{if(pt_users.indexOf(a)!=-1){pt_redirectBack()}}function pt_redirectBack(){alert('Ez egy privát téma, nincs jogosultságod az olvasásához!');history.go(-1)}});
