52858d85dbb5979731ff66df32753ecdada88159
Gera Janos <gerajanos@gmail.com>
2011-11-14 17:29:44 +0100

    added hide logos script

---
+++ hide_logos/minified.js
@@ -0,0 +1,10 @@
+// SG.hu hide_logos v1.0 20111114
+// Készítette: dzsani
+// Inspiráció: cSuwwi - Drom - http://drom.hu/
+
+// Ezzel a script eltávolítja az összes logót
+// és a felhasználónevet teszi a helyére.
+
+
+// EZT A RÉSZT NE MÓDOSÍTSD !!!
+$(document).ready(function(){$(".topichead").each(function(){if($(this).find('a[href*="forumuserinfo.php"] img').length){var a=$(this).find('a[href*="forumuserinfo.php"] img').attr("title");a=a.replace(" - VIP","");$(this).find('a[href*="forumuserinfo.php"] img').remove();$(this).find('a[href*="forumuserinfo.php"]').html(a);$(this).find('a[href*="forumuserinfo.php"]').before(" © ")}})})
---
+++ hide_logos/source.js
@@ -0,0 +1,31 @@
+// SG.hu hide_logos v1.0 20111114
+// Készítette: dzsani
+// Inspiráció: cSuwwi - Drom - http://drom.hu/
+
+// Ezzel a script eltávolítja az összes logót
+// és a felhasználónevet teszi a helyére.
+
+
+// EZT A RÉSZT NE MÓDOSÍTSD !!!
+$(document).ready(function() {
+	
+	$('.topichead').each(function() {
+		
+		// Comments with logo image
+		if($(this).find('a[href*="forumuserinfo.php"] img').length) {
+		
+			// Find nickname
+			var nick = $(this).find('a[href*="forumuserinfo.php"] img').attr('title');
+				nick = nick.replace(" - VIP","");
+			
+			// Remove logo
+			$(this).find('a[href*="forumuserinfo.php"] img').remove();
+			
+			// Insert username
+			$(this).find('a[href*="forumuserinfo.php"]').html(nick);
+			
+			// Insert copyright
+			$(this).find('a[href*="forumuserinfo.php"]').before('&nbsp;&copy;&nbsp;');
+		}
+	});
+});
