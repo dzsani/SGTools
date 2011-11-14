7d7ef2f99a2d6c29e8765228ff621483c9f08e0d
Gera Janos <gerajanos@gmail.com>
2011-11-14 16:05:25 +0100

    added simplified disable point system code

--- disable_point_system/minified.js
+++ disable_point_system/minified.js
@@ -5,4 +5,12 @@
 // Ezzel a scripttel teljesen kikapcsolhatod a
 // pontrendszert.
 
-function dps_valaszmsg(a,b,c,d){if($("#"+a).css("display")!="block"){var e="/listazas_egy.php3?callerid=2&id="+b+"&no="+c;$.get(e,function(b){$("#"+a).html(b).show();dps_hidePointButtons();dps_overrideQuotes()})}else{$("#"+a).hide()}}function dps_overrideQuotes(){$(".msg-replyto a").unbind("click").click(function(e){e.preventDefault();var _params=$(this).attr("href").split(":");eval("dps_"+_params[1]+"")})}function dps_hidePointButtons(){$('.topichead .ertekelkep, .topichead span[id*="rates"]').hide();$(".msg-text").show();$(".msg-text").each(function(){if($(this).next().attr("id")=="leful"){$(this).next().hide()}})}$(document).ready(function(){dps_hidePointButtons();dps_overrideQuotes()})
+<style type="text/css">
+	.ertekelkep { display: none; }
+</style>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.msg-text').next().click();
+	});
+</script>
--- disable_point_system/source.js
+++ disable_point_system/source.js
@@ -5,67 +5,12 @@
 // Ezzel a scripttel teljesen kikapcsolhatod a
 // pontrendszert.
 
-function dps_hidePointButtons() {
+<style type="text/css">
+	.ertekelkep { display: none; }
+</style>
 
-	// Hide the point buttons
-	$('.topichead .ertekelkep, .topichead span[id*="rates"]').hide();
-	
-	// Show the messages text
-	$('.msg-text').show();
-	
-	// Hide show buttons
-	$('.msg-text').each(function() {
-		if( $(this).next().attr('id') == 'leful') {
-			$(this).next().hide();
-		}
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.msg-text').next().click();
 	});
-}
-
-function dps_overrideQuotes() {
-
-	$('.msg-replyto a').unbind('click').click(function(e) {
-	
-		// Prevent default submisson
-		e.preventDefault();
-		
-		// Get original link params
-		var _params = $(this).attr('href').split(':');
-		
-		// Run replacement funciton
-		eval('dps_'+_params[1]+'');
-	}); 
-}
-
-function dps_valaszmsg(target, id, no, callerid) {
-
-	// Show the message
-	if ($('#'+target).css('display') != 'block') {
-		
-		// URL to call
-		var url = '/listazas_egy.php3?callerid=2&id=' + id + '&no=' + no;
-		
-		// Make the call
-		$.get(url, function(data) {
-
-			// Show the comment
-			$('#'+target).html(data).show();
-			
-			// Hide the buttons
-			dps_hidePointButtons();
-			
-			// Override quotes
-			dps_overrideQuotes();
-		});
-	
-	// Hide the message
-	} else { $('#'+target).hide(); }
-}
-
-$(document).ready(function() {
-	
-	// Hide buttons on page load
-	dps_hidePointButtons();
-	
-	// Override quotes
-	dps_overrideQuotes();
-});
+</script>
